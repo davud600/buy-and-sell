8a76d698db480913761e7737fd0028f02cf0d9e0
davud600 <davudgorqi@gmail.com>
2022-07-04 12:10:36 +0300

    navbar logo text

--- comps/NavBar.tsx
+++ comps/NavBar.tsx
@@ -7,31 +7,14 @@ export default function NavBar() {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false)
 
   return (
-    <nav className="relative z-10 py-2.5 bg-white shadow-lg">
-      <div className="container flex flex-wrap items-center justify-center mx-auto">
+    <nav className="top-0 z-10 py-2.5 bg-white shadow-lg fixed w-screen px-2">
+      <div className="container flex items-center justify-center mx-auto">
         <Link href="/" className="flex items-center">
-          <span className="self-center text-xl font-semibold cursor-pointer whitespace-nowrap dark:text-white">
+          <span className="self-center text-sm font-bold cursor-pointer sm:text-xl whitespace-nowrap">
             Buy &amp; Sell
           </span>
         </Link>
-        <div className="flex w-9/12 ml-8 md:order-2">
-          {/* <button
-            type="button"
-            data-collapse-toggle="mobile-menu-3"
-            aria-controls="mobile-menu-3"
-            aria-expanded="false"
-            className="md:hidden text-gray-500 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-700 rounded-lg text-sm p-2.5 mr-1">
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg">
-              <path
-                fillRule="evenodd"
-                d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                clipRule="evenodd"></path>
-            </svg>
-          </button> */}
+        <div className="flex w-9/12 ml-2 sm:ml-4 md:order-2">
           <div className="relative w-full">
             <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
               <svg
@@ -55,7 +38,7 @@ export default function NavBar() {
           <button
             data-collapse-toggle="mobile-menu-3"
             type="button"
-            className="inline-flex items-center p-2 ml-8 text-sm text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none"
+            className="inline-flex items-center ml-2 text-gray-500 rounded-lg p sm:p-2 sm:ml-4 hover:bg-gray-100 focus:outline-none"
             aria-controls="mobile-menu-3"
             aria-expanded="false"
             onClick={() => setIsSidebarOpen(true)}>
