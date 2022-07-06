257de60ca2891ef971208d76a03c4b436c1ae80b
davud600 <davudgorqi@gmail.com>
2022-07-06 20:41:12 +0300

    sidebar animated

--- comps/NavBar.tsx
+++ comps/NavBar.tsx
@@ -66,7 +66,7 @@ export default function NavBar() {
         </div>
       </div>
       <Modal open={isSidebarOpen} onClose={() => setIsSidebarOpen(false)}>
-        <SidebarMenu onClose={() => setIsSidebarOpen(false)} />
+        <SidebarMenu />
       </Modal>
     </nav>
   )
--- comps/SidebarMenu.tsx
+++ comps/SidebarMenu.tsx
@@ -1,15 +1,23 @@
+import { useEffect, useRef } from 'react'
 import Link from 'next/link'
 
-export default function SidebarMenu({ onClose }: any) {
+export default function SidebarMenu() {
+  const sidebarElement: any = useRef()
+
+  useEffect(() => {
+    const sidebarCurrent = sidebarElement.current
+    sidebarCurrent.style.right = '-300px'
+
+    setTimeout(() => {
+      sidebarCurrent.style.right = '0'
+    }, 0)
+  }, [])
+
   return (
-    <div className="fixed inset-y-0 right-0 z-10 m-0 bg-white w-72">
-      <div className="text-white bg-gray-800 h-44">
-        <button
-          className="fixed right-0 mr-2 text-xl font-bold"
-          onClick={onClose}>
-          x
-        </button>
-      </div>
+    <div
+      ref={sidebarElement}
+      className="absolute inset-y-0 right-0 z-10 m-0 transition-all bg-white sidebar-menu w-72">
+      <div className="text-white bg-gray-800 h-44"></div>
       <div className="flex flex-col divide-y-2">
         <div className="flex flex-col">
           <Link href="/">
@@ -22,19 +30,19 @@ export default function SidebarMenu({ onClose }: any) {
         </div>
         <div className="flex flex-col">
           <Link href="/">
-            <div className="p-3 cursor-pointer hover:bg-gray-200">
+            <div className="p-3 transition-colors cursor-pointer hover:bg-gray-200">
               <span className="px-4"></span>
               <span className="text-sm">About us</span>
             </div>
           </Link>
           <Link href="/">
-            <div className="p-3 cursor-pointer hover:bg-gray-200">
+            <div className="p-3 transition-colors cursor-pointer hover:bg-gray-200">
               <span className="px-4"></span>
               <span className="text-sm">Terms and conditions</span>
             </div>
           </Link>
           <Link href="/">
-            <div className="p-3 cursor-pointer hover:bg-gray-200">
+            <div className="p-3 transition-colors cursor-pointer hover:bg-gray-200">
               <span className="px-4"></span>
               <span className="text-sm">Privacy Policy</span>
             </div>
