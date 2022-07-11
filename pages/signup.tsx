626e8559eff9cee4751deeee8c18ea109ff73b18
davud600 <davudgorqi@gmail.com>
2022-07-11 15:33:18 +0300

    signup page style

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 
-export default function login() {
+export default function signup() {
   return (
     <section className="h-screen">
       <div className="h-full px-6 text-gray-800">
@@ -15,7 +15,7 @@ export default function login() {
           <div className="mb-12 xl:ml-20 xl:w-5/12 lg:w-5/12 md:w-8/12 md:mb-0">
             <form>
               <p className="mb-8 text-2xl font-normal leading-snug text-center uppercase">
-                Log in
+                Sign up
               </p>
               <div className="mb-6">
                 <input
@@ -35,35 +35,26 @@ export default function login() {
                 />
               </div>
 
-              <div className="flex items-center justify-between mb-6">
-                <div className="form-group form-check">
-                  <input
-                    type="checkbox"
-                    className="float-left w-4 h-4 mt-1 mr-2 align-top transition duration-200 bg-white bg-center bg-no-repeat bg-contain border border-gray-300 rounded-sm appearance-none cursor-pointer form-check-input checked:bg-gray-700 checked:border-blue-600 focus:outline-none"
-                    id="exampleCheck2"
-                  />
-                  <label className="inline-block text-gray-800 form-check-label">
-                    Remember me
-                  </label>
-                </div>
-                <Link href="/reset-password">
-                  <span className="text-red-600 cursor-pointer">
-                    Forgot password?
-                  </span>
-                </Link>
+              <div className="mb-6">
+                <input
+                  type="password"
+                  className="block w-full px-4 py-2 m-0 text-xl font-normal text-gray-700 transition ease-in-out bg-white border border-gray-300 border-solid rounded-lg form-control bg-clip-padding focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none"
+                  id="exampleFormControlInput2"
+                  placeholder="Confirm Password"
+                />
               </div>
 
               <div className="text-center lg:text-left">
                 <button
                   type="button"
                   className="inline-block py-3 text-sm font-medium leading-snug text-white uppercase transition duration-150 ease-in-out bg-gray-700 rounded-lg shadow-md px-7 hover:bg-gray-800 hover:shadow-lg focus:bg-gray-800 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-gray-900 active:shadow-lg">
-                  Log in
+                  Sign up
                 </button>
                 <p className="pt-1 mt-2 mb-0 text-sm font-semibold">
-                  Dont have an account? &nbsp;
-                  <Link href="/signup">
+                  Already have an account? &nbsp;
+                  <Link href="/login">
                     <span className="text-red-600 transition duration-200 ease-in-out cursor-pointer hover:text-red-700 focus:text-red-700">
-                      Register
+                      Log in
                     </span>
                   </Link>
                 </p>
